3042e9e398b4be47b2294f34d95a11d4b0025af5
4Jolly <51079536+4Jolly@users.noreply.github.com>
2020-08-04 23:52:56 +0200

    translations.js
    Add italian language

---
+++ translations.js
@@ -0,0 +1,58 @@
+// Add additional languages with their ISO 639-1 language code
+
+const translations = {
+  en: {
+    placeholder: {
+      tts: 'Text to speak',
+    },
+    label: {
+      leave: 'Leave',
+      ungroup: 'Ungroup',
+      group_all: 'Group all',
+      send: 'Send',
+    },
+    state: {
+      idle: 'Idle',
+      unavailable: 'Unavailable',
+    },
+    title: {
+      speaker_management: 'Group management',
+    },
+  },
+  fr: {
+    placeholder: {
+      tts: 'Texte à lire',
+    },
+    label: {
+      leave: 'Quitter',
+      ungroup: 'Dégrouper',
+      group_all: 'Grouper tous',
+      send: 'Envoyer',
+    },
+    state: {
+      idle: 'Inactif',
+      unavailable: 'Indisponible',
+    },
+    title: {
+      speaker_management: 'Gestion des groupes',
+    },
+  },
+  it: {
+    placeholder: {
+      tts: 'Testo da leggere',
+    },
+    label: {
+      leave: 'Uscire',
+      ungroup: 'Separa',
+      group_all: 'Raggruppa tutto',
+      send: 'Invia',
+    },
+    state: {
+      idle: 'Inattivo',
+      unavailable: 'Non disponibile',
+    },
+    title: {
+      speaker_management: 'Gestione del gruppo',
+    },
+  },
+};
